74bb3bbe9bf172e7b331486289ace0b60443dbd4
PeterSmart Link <petersmartlink@gmail.com>
2026-09-07 19:15:56 +0300

    Fix Now Playing artwork contract lint

--- test/now_playing_artwork_safety_contract_test.dart
+++ test/now_playing_artwork_safety_contract_test.dart
@@ -10,11 +10,11 @@ void main() {
 
     expect(source, contains('_maxArtworkBytes = 5 * 1024 * 1024'));
     expect(source, contains('_maxCachedArtworkFiles = 24'));
-    expect(source, contains("final client = http.Client()"));
-    expect(source, contains("final request = http.Request('GET', uri)"));
+    expect(source, contains('final client = http.Client()'));
+    expect(source, contains('final request = http.Request(\'GET\', uri)'));
     expect(source, contains('await for (final chunk in response.stream)'));
     expect(source, contains('received > _maxArtworkBytes'));
-    expect(source, contains("File('${target.path}.part')"));
+    expect(source, contains('File(\'\${target.path}.part\')'));
     expect(source, contains('_pruneArtworkCache'));
     expect(source, contains('albumArtBytes.length > _maxArtworkBytes'));
     expect(source, isNot(contains('response.bodyBytes')));
