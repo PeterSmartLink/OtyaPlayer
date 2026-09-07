6e513116484b2cbf3c6d83772ef7f7434698d4e4
PeterSmart Link <petersmartlink@gmail.com>
2026-09-07 15:50:09 +0300

    test: fix release size contract literals

--- test/release_size_budget_contract_test.dart
+++ test/release_size_budget_contract_test.dart
@@ -8,14 +8,14 @@ void main() {
 
     expect(publisher, contains('MAX_APK_BYTES=40000000'));
     expect(publisher, contains('WARN_APK_BYTES=35000000'));
-    expect(publisher, contains('[ "$SIZE" -le "$MAX_APK_BYTES" ]'));
+    expect(publisher, contains(r'[ "$SIZE" -le "$MAX_APK_BYTES" ]'));
     expect(
       publisher,
-      contains('Otya split APKs must stay at or below $MAX_APK_BYTES bytes'),
+      contains(r'Otya split APKs must stay at or below $MAX_APK_BYTES bytes'),
     );
     expect(
-      publisher.indexOf('[ "$SIZE" -le "$MAX_APK_BYTES" ]'),
-      lessThan(publisher.indexOf('upload_and_verify "$ARM64_APK"')),
+      publisher.indexOf(r'[ "$SIZE" -le "$MAX_APK_BYTES" ]'),
+      lessThan(publisher.indexOf(r'upload_and_verify "$ARM64_APK"')),
     );
   });
 }
